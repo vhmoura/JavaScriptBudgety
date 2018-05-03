ef9e7cb489652f594fcaf155858e0476e0268699
Vinicios De Moura <Vinicios.DeMoura@bglgroup.co.uk>
2018-05-03 09:22:38 +0100

    first modules

---
+++ app.js
@@ -0,0 +1,27 @@
+var budgeController = (function() {
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+     publicTest : function(b) {
+         return add(b);
+     }
+    }
+})();
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+   var z = budgetCtrl.publicTest(5);
+   return {
+       anotherPublic: function() {
+           console.log(z);
+       }
+   }
+})(budgeController, UIController);
